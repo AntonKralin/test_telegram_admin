919ca4888c02c4a169d76d06d6654bc318c31611
AntonKralin <antonkralin91@gmail.com>
2023-01-06 14:29:46 +0300

    test

--- classes/base_dao.py
+++ classes/base_dao.py
@@ -1,9 +1,11 @@
+from abc import ABC, abstractmethod
 from sqlalchemy.orm import Session
 
-class BaseDAO:
+class BaseDAO(ABC):
     def init(self, session: Session):
         self.session = session
 
+    @abstractmethod
     def insert(self, obj):
         session = self.s_maker()
         try:
@@ -14,6 +16,7 @@ class BaseDAO:
         finally:
             session.close()
 
+    @abstractmethod
     def update(self, obj):
         session = self.s_maker()
         try:
@@ -24,12 +27,14 @@ class BaseDAO:
         finally:
             session.close()
 
+    @abstractmethod
     def find_by_id(self, id):
         pass
 
     def find_all(self):
         pass
 
+    @abstractmethod
     def dell(self, obj):
         session = self.s_maker()
         try:
